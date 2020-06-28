1a6461fac5e27473b94d5dc04fbda795d941e401
Tagizade Orxan <tagizade.orxan@gmail.com>
2020-06-28 22:12:02 +0400

    scrolling solved

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ import addMessage, { jsonFile } from './messages.js';
 localStorage.setItem('username','anonim');
 
 let result;
-let messages = document.querySelector("#messages");
+let messages = document.getElementById("messages");
 
 let showResult = 0;
 
@@ -14,8 +14,10 @@ fetch(`https://5ea3c7e4270de6001645fbd1.mockapi.io/Messages`)
 
 
     messages.innerHTML="";
-fetch(`https://5ea3c7e4270de6001645fbd1.mockapi.io/Messages`)
+let fetched = fetch(`https://5ea3c7e4270de6001645fbd1.mockapi.io/Messages`)
 .then(res=>res.json()).then(res=> {
+
+    
     res.map(e=> {
        
         let message = document.createElement('p');
@@ -23,8 +25,25 @@ fetch(`https://5ea3c7e4270de6001645fbd1.mockapi.io/Messages`)
         message.innerHTML = `<span style="color:${color}">${e.name}</span> 
         ${e.message}`
         messages.append(message)
-    })
+
+      
+        
+    } 
+    
+    )
+    
 })
+
+fetched.then(res=>{
+    var element = document.getElementById("messages");
+    window.scrollTo(0, element.scrollHeight);
+})
+
+
+
+
+
+
 
 setInterval(()=>{
     fetch(`https://5ea3c7e4270de6001645fbd1.mockapi.io/Messages`)
@@ -38,8 +57,11 @@ setInterval(()=>{
                 message.innerHTML = `<span style="color:${color}">${res[i].name}</span> 
                 ${res[i].message}`
                 messages.append(message)
+                
               }
               showResult = res.length;
+              var element = document.getElementById("messages");
+    window.scrollTo(0, element.scrollHeight);
 
               if(res.length >99) {
                 let deleteOption = {
@@ -62,16 +84,22 @@ setInterval(()=>{
 
 
 
-    
-   
-    
+window.onload=function () {
+    var element = document.getElementById("messages");
+    console.log(element.scrollHeight)
+    element.scrollTop = element.scrollHeight;
+    window.scrollTo(0, 500);
+}
+
+window.scrollTop = window.height;
+
 
 let messageSend = (e) => {
     if(e.key === "Enter") {
         let message = e.target.parentNode.children[0].value;
         result = addMessage(localStorage.getItem('username'), message);
         messageInput.value="";
-   
+       
     }    
     }
 
